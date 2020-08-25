7d1fb6376c73a96f8bf8331cc4058bee7f6fb10e
ashleshmahule <ashleshmahule15@gmail.com>
2020-08-25 18:54:19 +0530

    init

---
+++ predict.py
@@ -0,0 +1,89 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+import pickle
+import numpy as np
+from tensorflow.python.keras.models import load_model
+model = load_model('chatbot_model.h5')
+
+import json
+import random
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+
+def clean_up_sentence(sentence):
+    # tokenization
+    sentence_words = nltk.word_tokenize(sentence)
+    # stemming
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=True):
+    # tokenization
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words) 
+    
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    
+    for r in res:
+        print((float(r)))
+
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+
+    # print('after error ----------------->')
+
+    # for r in res:
+    #     print((float(r)))
+
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+
+    # print('after sort ----------------->')
+    # print((results[0]))
+    # for r in res:
+    #     print((float(r)))
+    
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+
+    #  print('returnlist ----------------->')
+    # for r in return_list:
+    #     print(r)
+
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['answers'])
+            break
+    return result
+def chatbot_response(text):
+    ints = predict_class(text, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+print(chatbot_response("need a doc"))
